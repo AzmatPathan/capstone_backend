365b74c629a450a03bfb42f220a96073e34e75cb
Azmat Pathan <azmat.08pathan@gmail.com>
2024-06-23 13:47:56 -0400

    reviewExport Controllet

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,5 +1,6 @@
 import { addEquipmentDataReview, assignReview, getAllReviews, getReviewDetailsById, updateReviewStatus } from '../models/reviewModel.js';
 import asyncHandler from '../middleware/asyncHandler.js';
+import { exportToCSV } from '../utils/exportToCsv.js';
 
 
 export const addEquipmentWithReview = asyncHandler(async (req, res) => {
@@ -97,3 +98,31 @@ export const getReviewDetailsController = asyncHandler(async (req, res) => {
         res.status(500).json({ success: false, message: 'Internal server error' });
     }
 });
+
+// @desc    Export all equipment data
+// @route   GET /api/dashboard/export/review
+// @access  Public
+export const exportReviewData = async (req, res) => {
+    try {
+        // Fetch equipment data from your service
+        const reviews = await getAllReviews();
+
+        // Define CSV fields based on your equipment data structure
+        const csvFields = [
+            { id: 'review_id', title: 'Review Id' },
+            { id: 'equipment_id', title: 'Equipment ID' },
+            { id: 'barcode', title: 'Barcode' },
+            { id: 'created_by', title: 'Created By' },
+            { id: 'created_at', title: 'Created At' },
+            { id: 'reviewed_by', title: 'Reviewed By' },
+            { id: 'reviewed_at', title: 'Reviewed At' },
+            { id: 'status', title: 'Status' }
+        ];
+
+        // Export data to CSV using the utility function
+        await exportToCSV(reviews, csvFields, 'reviews.csv', res);
+    } catch (error) {
+        console.error('Error exporting equipment data:', error);
+        res.status(500).json({ message: 'Failed to export equipment data as CSV' });
+    }
+};
--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { exportEquipmentData, getEquipmentDashboard } from '../controllers/equipmentController.js';
+import { exportReviewData } from '../controllers/reviewController.js';
 
 const router = express.Router();
 
 router.get('/', getEquipmentDashboard);
 router.get('/export/equipment', exportEquipmentData);
+router.get('/export/review', exportReviewData);
 
 export default router;
